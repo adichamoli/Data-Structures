4484c96cd91ffb9a3c5951b7b6fcb8e0a5f6fd92
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-01-10 12:37:27 +0530

    Data Structures

---
+++ Algorithms/03-Queue/C#/LinkedListQueue/LinkedListQueue.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace LinkedListQueue
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    class Queue
+    {
+        Node front, rear;
+
+        public Queue()
+        {
+            this.front = this.rear = null;
+        }
+
+        public void Enqueue(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.rear == null)
+            {
+                this.rear = this.front = node;
+                return;
+            }
+
+            this.rear.next = node;
+            this.rear = node;
+        }
+
+        public void Dequeue()
+        {
+            if (this.front == null)
+                return;
+
+            Node node = this.front;
+            this.front = this.front.next;
+
+            if (this.front == null)
+                this.rear = null;
+        }
+
+        public void Display()
+        {
+            if (this.front == null)
+            {
+                Console.WriteLine("The Queue is empty");
+                return;
+            }
+
+            Node temp = this.front;
+
+            while (temp != null)
+            {
+                Console.WriteLine("Element : " + temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ Algorithms/03-Queue/C#/LinkedListQueue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListQueue
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue(10);
+            queue.Enqueue(20);
+            queue.Display();
+            
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Enqueue(30);
+            queue.Enqueue(40);
+            queue.Enqueue(50);
+            queue.Display();
+            
+            queue.Dequeue();
+            queue.Display();
+        }
+    }
+}
