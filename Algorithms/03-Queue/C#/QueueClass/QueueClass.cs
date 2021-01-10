eb29458635c03a085ecc5d6ebd2e445361e46bbb
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-01-10 13:05:36 +0530

    Data Structures

---
+++ Algorithms/03-Queue/C#/QueueClass/QueueClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+namespace QueueClass
+{
+    class QueueClass
+    {
+        static void Display(Queue queue)
+        {
+            foreach (Object obj in queue)
+            {
+                Console.WriteLine("Element : " + obj);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+
+            queue.Enqueue(10);
+            queue.Enqueue(20);
+            queue.Enqueue(30);
+
+            Display(queue);
+            Console.WriteLine("The number of elements in the Queue : " + queue.Count);
+
+            queue.Dequeue();
+
+            Display(queue);
+
+            Console.WriteLine("The number of elements in the Queue : " + queue.Count);
+            Console.WriteLine("Does the Queue contain : " + queue.Contains(3));
+
+        }
+    }
+}
