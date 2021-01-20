45cee88378c125f7eb4905585e3c1c9863d3b7e7
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-01-21 00:04:44 +0530

    Data Structures

--- Algorithms/05-Sorting/C/007-RadixSort.c
+++ Algorithms/05-Sorting/C/007-RadixSort.c
@@ -1,72 +1,65 @@
-// C++ implementation of Radix Sort 
-#include<iostream> 
+#include <stdio.h>
 
-// A utility function to get maximum value in arr[] 
-int getMax(int arr[], int n) 
-{ 
-	int mx = arr[0]; 
-	for (int i = 1; i < n; i++) 
-		if (arr[i] > mx) 
-			mx = arr[i]; 
-	return mx; 
-} 
+int getMax(int arr[], int size)
+{
+    int max = arr[0];
+    int i = 0;
+    for( i = 0 ; i < size ; i++ )
+        if( arr[i] > max )
+            max = arr[i];
+            
+    return max;
+}
 
-// A function to do counting sort of arr[] according to 
-// the digit represented by exp. 
-void countSort(int arr[], int n, int exp) 
-{ 
-	int output[n]; // output array 
-	int i, count[10] = {0}; 
+void countSort(int arr[], int size, int exp)
+{
+    int output[size];
+    int i = 0;
+    int count[10] = {0};
+    
+    for ( i = 0 ; i < size ; i++ )
+        count[(arr[i]/exp) % 10]++;
+        
+    for ( i = 1 ; i < 10 ; i++ )
+        count[i] += count[i - 1];
+        
+    for ( i = size - 1 ; i >= 0 ; i-- )
+    {
+        output[count[(arr[i] / exp) % 10 ] - 1] = arr[i];
+        count[(arr[i] / exp) % 10 ]--;
+    }
 
-	// Store count of occurrences in count[] 
-	for (i = 0; i < n; i++) 
-		count[ (arr[i]/exp)%10 ]++; 
+    for ( i = 0 ; i < size ; i++ )
+        arr[i] = output[i];
+}
 
-	// Change count[i] so that count[i] now contains actual 
-	// position of this digit in output[] 
-	for (i = 1; i < 10; i++) 
-		count[i] += count[i - 1]; 
+void radixSort(int arr[], int size)
+{
+    int max = getMax(arr, size);
+    int exp = 0;
+    
+    for ( exp = 1 ; max / exp > 0 ; exp *= 10)
+        countSort(arr, size, exp);
+}
 
-	// Build the output array 
-	for (i = n - 1; i >= 0; i--) 
-	{ 
-		output[count[ (arr[i]/exp)%10 ] - 1] = arr[i]; 
-		count[ (arr[i]/exp)%10 ]--; 
-	} 
+void display(int arr[], int size)
+{
+    int i = 0;
+    for(i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+}
 
-	// Copy the output array to arr[], so that arr[] now 
-	// contains sorted numbers according to current digit 
-	for (i = 0; i < n; i++) 
-		arr[i] = output[i]; 
-} 
-
-// The main function to that sorts arr[] of size n using 
-// Radix Sort 
-void radixsort(int arr[], int n) 
-{ 
-	// Find the maximum number to know number of digits 
-	int m = getMax(arr, n); 
-
-	// Do counting sort for every digit. Note that instead 
-	// of passing digit number, exp is passed. exp is 10^i 
-	// where i is current digit number 
-	for (int exp = 1; m/exp > 0; exp *= 10) 
-		countSort(arr, n, exp); 
-} 
-
-// A utility function to print an array 
-void print(int arr[], int n) 
-{ 
-	for (int i = 0; i < n; i++) 
-		cout << arr[i] << " "; 
-} 
-
-// Driver program to test above functions 
 int main() 
 { 
-	int arr[] = {170, 45, 75, 90, 802, 24, 2, 66}; 
-	int n = sizeof(arr)/sizeof(arr[0]); 
-	radixsort(arr, n); 
-	print(arr, n); 
-	return 0; 
+    int arr[] = {64, 25, 12, 22, 11}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    
+    printf("Unsorted array: \n");
+    display(arr, n);
+    printf("\n");
+    
+    radixSort(arr, n); 
+    printf("Sorted array: \n"); 
+    display(arr, n); 
+    return 0; 
 } 
