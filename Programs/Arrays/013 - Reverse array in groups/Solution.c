c9a8bdde63e485254835a44b7d43d7b1845cfb6b
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-01-29 19:34:12 +0530

    Data Structures

---
+++ Programs/Arrays/013 - Reverse array in groups/Solution.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+     
+void swap(int *xp, int *yp) 
+{ 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+}
+
+void reverse(int arr[], int n, int k) 
+{ 
+    for (int i = 0; i < n; i += k) 
+    { 
+        int left = i; 
+  
+        int right = min(i + k - 1, n - 1); 
+  
+        while (left < right) 
+            swap(&arr[left++], &arr[right--]); 
+    } 
+}
+
+int main() 
+{ 
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8}; 
+    int k = 3; 
+  
+    int n = sizeof(arr) / sizeof(arr[0]); 
+  
+    reverse(arr, n, k); 
+  
+    for (int i = 0; i < n; i++) 
+        printf("%d ",arr[i]); 
+  
+    return 0; 
+}
