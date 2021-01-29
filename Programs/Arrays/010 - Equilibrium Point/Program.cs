b6b114af2229d9b2fe572b265844e503d98e316b
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-01-29 16:43:41 +0530

    Data Structures

---
+++ Programs/Arrays/010 - Equilibrium Point/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EquilibriumPoint
+{
+    class Program
+    {
+        static int equilibrium(int[] arr, int n)
+        {
+            int sum = 0;
+            int leftsum = 0;
+
+            for (int i = 0; i < n; ++i)
+                sum += arr[i];
+
+            for (int i = 0; i < n; ++i)
+            {
+                sum -= arr[i];
+
+                if (leftsum == sum)
+                    return i + 1;
+
+                leftsum += arr[i];
+            }
+
+            return -1;
+        }
+
+        public static void Main()
+        {
+            int[] arr = { 1, 3, 5, 2, 2 };
+            int arr_size = arr.Length;
+
+            Console.Write("First equilibrium index is " +
+                               equilibrium(arr, arr_size));
+        }
+    }
+}
