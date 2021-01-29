a4004004ef71c0c665fd2e3e43bf1d6b8cc01e14
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-01-29 19:14:58 +0530

    Data Structures

---
+++ Programs/Arrays/012 - Minimum Platforms/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace PlatformNeeded
+{
+    class Program
+    {
+        static int FindPlatform(int[] arr, int[] dep, int n)
+        {
+            Array.Sort(arr);
+            Array.Sort(dep);
+
+            int plat_needed = 1, result = 1;
+            int i = 1, j = 0;
+
+            while (i < n && j < n)
+            {
+                if (arr[i] <= dep[j])
+                {
+                    plat_needed++;
+                    i++;
+                }
+
+                else if (arr[i] > dep[j])
+                {
+                    plat_needed--;
+                    j++;
+                }
+
+                if (plat_needed > result)
+                    result = plat_needed;
+            }
+
+            return result;
+        }
+ 
+        public static void Main()
+        {
+            int[] arr = { 900, 940, 950, 1100, 1500, 1800 };
+            int[] dep = { 910, 1200, 1120, 1130, 1900, 2000 };
+
+            int n = arr.Length;
+            Console.Write("Minimum Number of Platforms Required = " + FindPlatform(arr, dep, n));
+        }
+    }
+}
