4627468858670032517e1469e10c637b92d7b64d
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-01-29 17:12:51 +0530

    Data Structures

---
+++ Programs/Arrays/011 - Leaders in an array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeadersInArray
+{
+    class Program
+    {
+        static void printLeaders(int[] arr, int size)
+        {
+            int max_from_right = arr[size - 1];
+
+            Console.Write(max_from_right + " ");
+
+            for (int i = size - 2; i >= 0; i--)
+            {
+                if (max_from_right <= arr[i])
+                {
+                    max_from_right = arr[i];
+                    Console.Write(max_from_right + " ");
+                }
+            }
+        }
+
+        public static void Main(String[] args)
+        {
+            int[] arr = new int[] { 16, 17, 4, 3, 5, 2 };
+            int n = arr.Length;
+            printLeaders(arr, n);
+        }
+    }
+}
