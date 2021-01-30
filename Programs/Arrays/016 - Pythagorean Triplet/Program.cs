8e535dd8be5279b100ebb07cb4a8c9dbe1764a6f
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-01-30 14:06:19 +0530

    Data Structures

---
+++ Programs/Arrays/016 - Pythagorean Triplet/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace PyhtagoreanTriplet
+{
+    class Program
+    {
+        static bool CheckTriplet(int[] arr, int n)
+        {
+            int maximum = 0;
+
+            for (int i = 0; i < n; i++)
+                maximum = Math.Max(maximum, arr[i]);
+
+            int[] hash = new int[maximum + 1];
+
+            for (int i = 0; i < n; i++)
+                hash[arr[i]]++;
+
+            for (int i = 1; i < maximum + 1; i++)
+            {
+                if (hash[i] == 0)
+                    continue;
+
+                for (int j = 1; j < maximum + 1; j++)
+                {
+                    if ((i == j && hash[i] == 1) || hash[j] == 0)
+                        continue;
+
+                    int val = (int)Math.Sqrt(i * i + j * j);
+
+                    if ((val * val) != (i * i + j * j))
+                        continue;
+
+                    if (val > maximum)
+                        continue;
+
+                    if (hash[val] == 1)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static void Main(String[] args)
+        {
+            int[] arr = { 3, 2, 4, 6, 5 };
+            int n = arr.Length;
+            if (CheckTriplet(arr, n))
+                Console.Write("Yes");
+            else
+                Console.Write("No");
+        }
+    }
+}
