cf39b7e48bbc45835b54d5450d9dbfee8bf119a1
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-01-30 13:58:42 +0530

    Data Structures

---
+++ Programs/Arrays/016 - Pythagorean Triplet/Solution.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+
+#define max(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+
+bool checkTriplet(int arr[], int n)
+{
+    int maximum = 0;
+    int hash[10];
+    int i = 0;
+    int val = 0;
+    int j = 0;
+    int length = maximum + 1;
+    
+    memset(hash, 0, 10);
+    
+    for ( i = 0 ; i < n ; i++ )
+    {
+        maximum = max(maximum, arr[i]);
+        hash[arr[i]] = 0;
+    }
+        
+    for ( i = 0 ; i < n ; i++ )
+        hash[arr[i]]++;
+        
+    for ( i = 1 ; i < maximum + 1 ; i++ )
+    {
+        if ( hash[i] == 0 )
+            continue;
+            
+        for ( j = 1 ; j < maximum + 1 ; j++ )
+        {
+            if (( i == j && hash[i] == 1) || hash[j] == 0 )
+                continue;
+                
+            val = sqrt( (i * i) + (j * j) );
+            
+            if ((val * val) != ((i * i) + (j * j)))
+                continue;
+                
+            if ( val > maximum )
+                continue;
+                
+            if ( hash[val])
+            {
+                return true;
+            }
+        }
+    }
+    
+    return false;
+}
+
+int main() 
+{ 
+    int arr[] = { 3, 8, 5 }; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    if (checkTriplet(arr, n)) 
+        printf("Yes"); 
+    else
+        printf("No"); 
+}
