33c9c175140dc47ba25bd0ac1ce58087d40b52c4
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-01 23:45:19 +0530

    Data Structures

---
+++ Programs/Arrays/023 - Largest Number formed from an Array/LargestNumber.py
@@ -0,0 +1,11 @@
+from itertools import permutations
+
+
+def largest(l):
+    lst = []
+    for i in permutations(l, len(l)):
+        lst.append("".join(map(str, i)))
+    return max(lst)
+
+
+print(largest([54, 546, 548, 60]))
