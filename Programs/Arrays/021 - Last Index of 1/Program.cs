b408c3ca8ee18aa67ced9685768f8420ffd81904
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-01 22:01:13 +0530

    Data Structures

---
+++ Programs/Arrays/021 - Last Index of 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LastIndexOf1
+{
+    class Program
+    {
+        public static int FindIndex(int[] arr, int idx, int K)
+        {
+            if (idx < 0)
+                return -1;
+
+            if (arr[idx] == K)
+            {
+                return idx;
+            }
+
+            return FindIndex(arr, idx - 1, K);
+        }
+
+        public static void Main(String[] args)
+        {
+            int[] arr = new int[] { 4, 3, 7, 8, 6, 2, 1 };
+            int result = FindIndex(arr, arr.Length - 1, 1);
+            Console.WriteLine("Last Index of 1 : " + result);
+        }
+    }
+}
