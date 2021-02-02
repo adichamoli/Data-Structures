41d8f7231f00c2cff24272a5cc151099708cf4df
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-02 19:37:31 +0530

    Data Structures

--- Programs/Strings/001 - Reverse words in a given string/Program.cs
+++ Programs/Strings/001 - Reverse words in a given string/Program.cs
@@ -4,6 +4,22 @@ namespace ReverseWords
 {
     class Program
     {
+        public static String[] RevString(String[] s, int l)
+        {
+            int j = l / 2;
+            
+            while (j <= l - 1)
+            {
+                String temp;
+                temp = s[l - j - 1];
+                s[l - j - 1] = s[j];
+                s[j] = temp;
+                j += 1;
+            }
+
+            return s;
+        }
+
         public static void Main()
         {
             string[] s = "i like this program very much".Split(' ');
@@ -14,8 +30,19 @@ namespace ReverseWords
                 ans += s[i] + " ";
             }
             
-            Console.Write("Reversed String:\n");
-            Console.Write(ans.Substring(0, ans.Length - 1));
+            Console.Write("Reversed String : ");
+            Console.WriteLine(ans.Substring(0, ans.Length - 1));
+
+            String s1 = "getting good at coding " +
+            "needs a lot of practice";
+            String[] words = s1.Split(' ');
+
+            words = RevString(words, words.Length);
+
+            s1 = String.Join(" ", words);
+
+            Console.WriteLine("Reversed String Without Space: ");
+            Console.WriteLine(s1);
         }
     }
 }
