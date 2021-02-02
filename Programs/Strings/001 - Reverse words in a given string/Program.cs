85f167c4e456c356eb994d89c63dce9db76583e9
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-02 19:12:28 +0530

    Data Structures

---
+++ Programs/Strings/001 - Reverse words in a given string/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReverseWords
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string[] s = "i like this program very much".Split(' ');
+            string ans = "";
+            
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                ans += s[i] + " ";
+            }
+            
+            Console.Write("Reversed String:\n");
+            Console.Write(ans.Substring(0, ans.Length - 1));
+        }
+    }
+}
